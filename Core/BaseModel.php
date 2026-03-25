2878af3fc9638000dd7e893ab82e348ed7f42b77
Romain CAMENEN <rcamenen@gmail.com>
2026-03-25 19:27:06 +0100

    Setting up the database connection singleton

--- Core/BaseModel.php
+++ Core/BaseModel.php
@@ -2,7 +2,19 @@
 namespace Core;
 
 abstract class BaseModel {
-    
+    protected $connection;
+
+    public function __construct() {
+        try{
+
+            $this->connection = Database::getInstance()->getConnection();
+
+        }catch(PDOException $e){
+
+            echo "Error BaseModel.php";
+
+        }
+    }
 }
 
 ?>
--- Core/Database.php
+++ Core/Database.php
@@ -3,7 +3,35 @@ namespace Core;
 use PDO;
 
 class Database {
-    
+    private static ?self $instance = null;
+    private PDO $connection;
+
+    private function __construct() {
+        try{
+
+            $this->connection = new PDO("mysql:
+                host=".$_ENV['DB_HOST'].";
+                dbname=".$_ENV['DB_NAME'],
+                $_ENV['DB_USER'],
+                $_ENV['DB_PASSWORD'],
+                [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+            );
+
+        }catch(Exception $e){
+            echo "Error Database.php";
+        }
+    }
+
+    public static function getInstance(): self {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection(): PDO {
+        return $this->connection;
+    }
 }
 
 ?>
