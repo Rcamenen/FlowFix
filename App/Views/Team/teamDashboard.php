b7e13b85bcc00a9ccf75de3bb741d17a8987d3e3
Romain CAMENEN <rcamenen@gmail.com>
2026-04-16 14:05:09 +0200

    Modification des cards et ajout de la tab cycle ald infos

--- App/Views/Team/teamDashboard.php
+++ App/Views/Team/teamDashboard.php
@@ -10,9 +10,9 @@
                     
             <?php unset($_SESSION['error']) ;} ?>
 
-<main class="main container" data-team-id=<?= $teamId ?>>
+<main id="teamDashboard" class="main container" data-team-id=<?= $teamId ?>>
 
-    <h1 class="title-md">Tableau de bord</h1>
+    <h1 class="title-md">Tableau de bord du groupe</h1>
     <!-- MESSAGE DE SUCCES OU D'ERREUR -->
 
     <?php if(!empty($_SESSION["error"])): ?>
@@ -25,11 +25,11 @@
     <div class="main__container">
     <aside class="main__nav">
         <nav class="subnav">
-            <p class="section-label">Groupe</p>
+            <p class="section-label"><?= $teamName ?></p>
             <ul class="subnav__list">
                 <li data-tab-button="dashboard" class="subnav__item btn-tab btn-tab--active">Vue globale</li>
                 <li data-tab-button="frictions" class="subnav__item btn-tab">Irritants</li>
-                <li data-tab-button="infos" class="subnav__item btn-tab">Infos groupe</li>
+                <li data-tab-button="infos" class="subnav__item btn-tab">Cycle</li>
                 <li data-tab-button="addFriction" class="subnav__item btn-primary--sm"><a href="/team/<?= $teamId ?>/friction/create">Créer un irritant</a></li>
             </ul>
         </nav>
@@ -43,6 +43,7 @@
             </div>
 
             <div class="section__content">
+
                 <?php foreach($frictionsToPilot as $f): ?>
                     <div class="frictionCard">
                         <h3><?= $f["title"] ?></h3>
@@ -87,6 +88,7 @@
             </div>
 
             <div class="section__content">
+
                 <?php foreach($frictionsInProgress as $f): ?>
                     <div class="frictionCard">
                         <h3><?= $f["title"] ?></h3>
@@ -114,10 +116,59 @@
             </div>
 
         </section>
+
+        <section data-tab-content="infos" class="main__section section dn main__section--first">
+
+            <div class="section__top">
+                <h2 class="section__title title-lg">Cycle en cours</h2>
+            </div>
+
+            <div class="section__content">
+
+
+                     <div class="card--cycle">
+
+                        <h3 class="title-md">Dates</h3>
+
+                        <div class="flex-col g-8">
+                            <p class="text--xs">Début : </p>
+                            <span class=" badge badge--inprogress"><?= new DateTime($cycle["start_date"])->format('d-m-Y') ?> </span>
+                        </div>
+
+                        <div class="flex-col g-8">
+                            <p class="text--xs">Fin :</p>
+                            <span class="badge badge--closed"><?= new DateTime($cycle["end_date"])->format('d-m-Y')?></span>
+                        </div>
+
+                        <p class="text--xs">Les cycles se terminent le jour indiqué à 23h59</p>
+                    </div>
+
+                    <!-- FORM POUR REQUETE API -->
+                    <div class="card--cycle">
+
+                        <h3 class="title-md">Consultation des jours fériés</h3>
+                        <p class="text--xs">Renseigner une date afin de savoir s'il s'agit d'un jour férié.</p>
+                        <form class="form" id="formHolidayCheck" action="">
+
+                            <input class="form__input" id="checkDate" name="checkDate" type="date" required>
+
+                            <button type="button" class="btn-primary">Vérifier</button>
+
+                            <p class="form__message"></p>
+
+                        </form>
+
+
+                    </div>
+
+            </div>       
+
+        </section>
     </div>
     </div>
 </main>
 
 <script src="/js/dashboard.js" defer></script>
+<script src="/js/nagerRequest.js" defer></script>
 
 <!-- <?php include(ROOT."/App/Views/Team/teamMember/teamMemberAddForm.php"); ?> -->
