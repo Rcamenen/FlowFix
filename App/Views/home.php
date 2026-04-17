f46d3575a2329d41ee6a7ea0f770050f77e9bd4a
Romain CAMENEN <rcamenen@gmail.com>
2026-04-17 10:54:56 +0200

    Modification du css pour mode desktop

--- App/Views/home.php
+++ App/Views/home.php
@@ -1,87 +1,74 @@
 
 <main id="home" class="main">
 
-    <!-- Message affiché à l'utilisateur -->
-
-
     <div class="main__content">
+            
+        <!--------------------------------------------------
 
+            HERO
+
+         -------------------------------------------------->
+    
         <div class="wrapper">
+            <section id="heroSection" class="main__section main__section--first section container">
 
-            <section class="main__section main__section--first section container">
+                <div class="left flex-col g-24">
 
-                <div class="section__top">
-                    <h1 class="title-xl">Résolvez les <span class="text--highlight">irritants</span> qui freinent votre équipe</h1>
+                    <div class="section__top">
+                        <h1 class="title-xl">Résolvez les <span class="text--highlight">irritants</span> qui freinent votre équipe</h1>
+                    </div>
+                    <div class="section__content">
+                        <p>FlowFix permet à chaque membre de l'équipe de signaler les frictions du quotidien, de voter collectivement pour les prioriser, et de les traiter cycle par cycle.</p>
+                    </div>
+                    <div class="section__bottom flex-col">
+                        <a class="btn-primary" href="/register">Créer un groupe</a>
+                        <a class="btn-secondary mt-16" href="#comment-ca-marche">Découvrir</a>
+                    </div>
+
+                    </div>
+
+                <div class="right">
+
+                    <div class="media">
+                        <center><p>video</p></center>
+                    </div>
+
                 </div>
-                <div class="section__content">
-                    <p>FlowFix permet à chaque membre de l'équipe de signaler les frictions du quotidien, de voter collectivement pour les prioriser, et de les traiter cycle par cycle.</p>
-                </div>
-                <div class="section__bottom flex-col">
-                    <a class="btn-primary" href="/register">Créer un groupe</a>
-                    <a class="btn-secondary mt-16" href="#comment-ca-marche">Découvrir</a>
-                </div>
+
 
             </section>
 
         </div>
+
+        <!--------------------------------------------------
+
+            COMMENT CA MARCHE
+
+         -------------------------------------------------->
 
         <div class="wrapper--alt">
 
             <section id=comment-ca-marche class="main__section section container">
 
                 <div class="section__top">
-                    <p class="section-label">Comment ça marche ?</p>
+                    <p class="section-label mb-16">Comment ça marche ?</p>
                     <h2 class="section__title title-xl">Simple. Collectif. Efficace.</h2>
                     <p class="text--sm">Un processus en trois étapes pour transformer les frustrations du quotidien en actions concrètes</p>
                 </div>
 
 
-                <ol class="list">
-                    <li>
+                <ol class="list grid">
+                    <li class="grid__item">
                         <h3 class="title-sm">1 - Signalez vos irritants</h3>
                         <p class="text--xs">Chaque membre peut ajouter un irritant au vivier du groupe à tout moment — une friction dans un processus, l'organisation ou la vie d'équipe.</p>
                     </li>
-                    <li>
+                    <li class="grid__item">
                         <h3 class="title-sm">2 - Votez collectivement</h3>
                         <p class="text--xs">Au début de chaque cycle, tous les membres disposent d'un nombre limité de voix pour prioriser ensemble les irritants à traiter en premier.</p>
                     </li>
-                    <li>
+                    <li class="grid__item">
                         <h3 class="title-sm">3 - Améliorez, cycle après cycle</h3>
                         <p class="text--xs">Les irritants les plus votés sont traités sur la durée du cycle. Les compteurs se remettent à zéro et un nouveau cycle commence.</p>
-                    </li>
-                </ol>
-
-            </section>
-
-        </div>
-
-        <div class="wrapper">
-
-            <section class="main__section section container">
-
-                <div class="section__top">
-                    <p class="section-label">Fonctionnalités</p>
-                    <h2 class="section__title title-xl">Tout ce qu'il faut, rien de superflu</h2>
-                    <p class="text--sm">FlowFix est conçu pour aller à l'essentiel : faciliter la parole, structurer l'action.</p>
-                </div>
-
-                <ul class="list">
-                    <li>
-                        <h3 class="title-sm">Groupes multi-membres</h3>
-                        <p class="text--xs">Chaque membre peut ajouter un irritant au vivier du groupe à tout moment — une friction dans un processus, l'organisation ou la vie d'équipe.</p>
-                    </li>
-                    <li>
-                        <h3 class="title-sm">Système de vote par cycle</h3>
-                        <p class="text--xs">Au début de chaque cycle, tous les membres disposent d'un nombre limité de voix pour prioriser ensemble les irritants à traiter en premier.</p>
-                    </li>
-                    <li>
-                        <h3 class="title-sm">Modération intégrée</h3>
-                        <p class="text--xs">Les irritants les plus votés sont traités sur la durée du cycle. Les compteurs se remettent à zéro et un nouveau cycle commence.
-                        </p>
-                    </li>
-                    <li>
-                        <h3 class="title-sm">Pensé mobile d'abord</h3>
-                        <p class="text--xs">Interface responsive optimisée en priorité pour mobile. Navigation fluide sans rechargement, menus accessibles en permanence, design sobre et lisible.</p>
                     </li>
                 </ul>
 
@@ -89,12 +76,54 @@
 
         </div>
 
+        <!--------------------------------------------------
+
+            FONCTIONNALTIÉ
+
+         -------------------------------------------------->
+
+        <div class="wrapper">
+
+            <section id="comment-ca-marche" class="main__section section container">
+
+                <div class="section__top">
+                    <p class="section-label mb-16">Fonctionnalités</p>
+                    <h2 class="section__title title-xl">Tout ce qu'il faut, rien de superflu</h2>
+                    <p class="text--sm">FlowFix est conçu pour aller à l'essentiel : faciliter la parole, structurer l'action.</p>
+                </div>
+
+                <ul class="grid">
+                    <li class="grid__item">
+                        <h3 class="title-sm">Groupes multi-membres</h3>
+                        <p class="text--xs">Chaque membre peut ajouter un irritant au vivier du groupe à tout moment — une friction dans un processus, l'organisation ou la vie d'équipe.</p>
+                    </li>
+                    <li class="grid__item">
+                        <h3 class="title-sm">Système de vote par cycle</h3>
+                        <p class="text--xs">Au début de chaque cycle, tous les membres disposent d'un nombre limité de voix pour prioriser ensemble les irritants à traiter en premier.</p>
+                    </li>
+                    <li class="grid__item">
+                        <h3 class="title-sm">Modération intégrée</h3>
+                        <p class="text--xs">Les irritants les plus votés sont traités sur la durée du cycle. Les compteurs se remettent à zéro et un nouveau cycle commence.
+                        </p>
+                    </li>
+                </ul>
+
+            </section>
+
+        </div>
+
+        <!--------------------------------------------------
+
+            prêt à commencer
+
+         -------------------------------------------------->
+
         <div class="wrapper--alt">
 
             <section id="pret-a-commencer" class="main__section section container">
 
                 <div class="section__top">
-                    <p class="section-label txt-center">Prêt à commencer ?</p>
+                    <p class="section-label txt-center  mb-16">Prêt à commencer ?</p>
                     <h2 class=" txt-center section__title title-xl">Donnez la parole à votre équipe</h2>
                     <p class=" txt-center">Créez votre premier groupe en quelques secondes...</p>
                 </div>
