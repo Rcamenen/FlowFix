dac6609b3c1be980f43280b9ce562a3b1878538e
Romain CAMENEN <rcamenen@gmail.com>
2026-04-19 10:50:26 +0200

    modification structure de la navbar, harmonisation des paddings avec le reste et centrage vertical des éléments.

--- App/Views/Layout/layout.php
+++ App/Views/Layout/layout.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,111 +8,124 @@
     <title>FlowFix</title>
 
     <link crossorigin="anonymous" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
-    integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
-    referrerpolicy="no-referrer" rel="stylesheet" />
+        integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
+        referrerpolicy="no-referrer" rel="stylesheet" />
 </head>
+
 <body>
     <script src="/js/nav.js" defer></script>
 
-    <?php if(isset($_GET["message"])) echo $_GET["message"]; ?>
+    <?php if (isset($_GET["message"])) echo $_GET["message"]; ?>
 
-    <?php if(isset($_SESSION["disconnect"])): ?>
-    <div class="container">
-        <p class="notice--success">Vous avez bien été déconnecté ! </p>
-        <?php unset($_SESSION["disconnect"]) ?>
-    </div>
+    <?php if (isset($_SESSION["disconnect"])): ?>
+        <div class="container">
+            <p class="notice--success">Vous avez bien été déconnecté ! </p>
+            <?php unset($_SESSION["disconnect"]) ?>
+        </div>
     <?php endif ?>
-    
+
     <header class="header">
 
-        <nav class="navbar">
 
-            <div class="navbar__logo">
-            FlowFix
+        <nav class="header __nav navbar">
+
+            <div class="container">
+                <div class="navbar__inner">
+
+                    <div class="navbar__logo">
+                        <p class="title-md">FlowFix</p>
+                    </div>
+
+                    <button aria-label="ouvrir le menu de navigation" class="navbar__burger">
+                        <i class="fa-solid fa-bars burger"></i>
+                    </button>
+
+                    <ul class="navbar__menu">
+
+                        <li class="navbar__item">
+                            <a class="navbar__link" href="/">Accueil</a>
+                        </li>
+
+                        <?php if (empty($_SESSION["adminId"])): ?>
+
+                            <?php if (!empty($_SESSION["userId"])): ?>
+
+                                <li class="navbar__item">
+                                    <a class="navbar__link" href="/teams">Groupes</a>
+                                </li>
+
+                                <li class="navbar__item">
+                                    <a class="navbar__link" href="/account">Profil</a>
+                                </li>
+
+                                <form action="/logout" method="post">
+                                    <button type="submit">Déconnexion</button>
+                                </form>
+
+                            <?php else: ?>
+
+                                <li class="navbar__item">
+                                    <a class="navbar__link" href="/register">Inscription</a>
+                                </li>
+
+                                <li class="navbar__item">
+                                    <a class="navbar__link" href="/login">Connexion</a>
+                                </li>
+
+                            <?php endif ?>
+
+                            <li class="navbar__item navbar__item--active">
+                                <a class="navbar__link" href="/contact">Contact</a>
+                            </li>
+
+                        <?php else: ?>
+
+                            <li class="navbar__item">
+                                <a class="navbar__link" href="/admin/teams">Groupes</a>
+                            </li>
+
+                            <li class="navbar__item navbar__item--active">
+                                <a class="navbar__link" href="/admin/users">Utilisateurs</a>
+                            </li>
+
+                            <li class="navbar__item navbar__item--active">
+                                <form action="/admin/logout" method="post">
+                                    <button class="navbar__link" type="submit">Déconnexion</button>
+                                </form>
+                            </li>
+
+                        <?php endif ?>
+
+                    </ul>
+                </div>
+
+
             </div>
 
-            <button aria-label="ouvrir le menu de navigation" class="navbar__burger">
-                <i class="fa-solid fa-bars burger"></i>
-            </button>
-
-            <ul class="navbar__menu">
-
-                <li class="navbar__item">
-                    <a class="navbar__link" href="/">Accueil</a>
-                </li>
-
-                <?php if(empty($_SESSION["adminId"])): ?>
-
-                    <?php if(!empty($_SESSION["userId"])): ?>
-
-                    <li class="navbar__item">
-                        <a class="navbar__link" href="/teams">Groupes</a>
-                    </li>
-
-                    <li class="navbar__item">
-                        <a class="navbar__link" href="/account">Profil</a>
-                    </li>
-
-                    <form action="/logout" method="post">
-                        <button type="submit">Déconnexion</button>
-                    </form>
-
-                    <?php else: ?>
-
-                    <li class="navbar__item">
-                        <a class="navbar__link" href="/register">Inscription</a>
-                    </li>
-
-                    <li class="navbar__item">
-                        <a class="navbar__link" href="/login">Connexion</a>
-                    </li>
-
-                    <?php endif ?>
-
-                    <li class="navbar__item navbar__item--active">
-                    <a class="navbar__link" href="/contact">Contact</a>
-                    </li>
-
-                <?php else: ?>
-
-                    <li class="navbar__item">
-                    <a class="navbar__link" href="/admin/teams">Groupes</a>
-                    </li>
-
-                    <li class="navbar__item navbar__item--active">
-                    <a class="navbar__link" href="/admin/users">Utilisateurs</a>
-                    </li>
-
-                    <li class="navbar__item navbar__item--active">
-                    <form action="/admin/logout" method="post">
-                        <button class="navbar__link" type="submit">Déconnexion</button>
-                    </form>
-                    </li>
-
-                <?php endif ?>
-
-            </ul>
-
         </nav>
+
+
+
 
     </header>
     <?php include($contentPath); ?>
     <footer class="footer">
 
         <div class="footer__inner container">
-            
+
             <div class="footer__brand">FlowFix</div>
-                    <div class="footer__nav">
-                        <ul>
-                            <li><a href="/contact">Contact</a></li>
-                            <li><a href="/legal">Mentions légales</a></li>
-                            <li><a href="/privacy">Politiques de confidentialité</a></li>
-                        </ul>
-                    </div>
+            <div class="footer__nav">
+                <ul>
+                    <li><a href="/contact">Contact</a></li>
+                    <li><a href="/legal">Mentions légales</a></li>
+                    <li><a href="/privacy">Politiques de confidentialité</a></li>
+                </ul>
+            </div>
             <div class="footer__copyright">&copy; FlowFix 2026</div>
 
         </div>
-        
+
     </footer>
 </body>
+
 </html>
