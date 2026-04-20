64a84d82c90092d25144578e5b4adce26c3f4c4d
Romain CAMENEN <rcamenen@gmail.com>
2026-04-20 11:52:38 +0200

    Ajout ligne pour debug php

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
 session_start();
 define('ROOT',__DIR__);
 
+ini_set('display_errors', '0');
+ini_set('log_errors', '1');
+ini_set('error_log', __DIR__ . '/errors.log');
+error_reporting(E_ALL);
+
 use Core\Router;
 use Dotenv\Dotenv;
 
