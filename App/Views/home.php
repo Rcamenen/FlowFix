272bcc2013b364f8f1615eda3b6cc90a0e90699b
Romain CAMENEN <rcamenen@gmail.com>
2026-04-22 11:26:26 +0200

    Remplacement du <i> par <span> pour icon menu burger

--- App/Views/home.php
+++ App/Views/home.php
@@ -1,20 +1,15 @@
 <div class="home">
 
-    <!--------------------------------------------------
-
-            HERO
-
-         -------------------------------------------------->
     <div class="page__content">
 
         <div class="wrapper">
 
-            <section id="heroSection" class="main__section main__section--first section container">
+            <section id="heroSection" class="main__section main__section--first section container" aria-labelledby="heroSectionTitle">
 
                 <div class="left flex-col g-24">
 
                     <div class="section__top">
-                        <h1 class="title-xl">Résolvez les <span class="text--highlight">irritants</span> qui freinent votre équipe</h1>
+                        <h1 id=heroSectionTitle class="title-xl">Résolvez les <span class="text--highlight">irritants</span> qui freinent votre équipe</h1>
                     </div>
                     <div class="section__content">
                         <p>FlowFix permet à chaque membre de l'équipe de signaler les frictions du quotidien, de voter collectivement pour les prioriser, et de les traiter cycle par cycle.</p>
@@ -37,23 +32,17 @@
                 </div>
 
             </section>
+
         </div>
 
 
-
-        <!--------------------------------------------------
-
-            COMMENT CA MARCHE
-
-         -------------------------------------------------->
-
         <div class="wrapper wrapper--alt">
 
-            <section id=comment-ca-marche class="main__section section container">
+            <section id=howItWork class="main__section section container" aria-labelledby="howItWorkTitle">
 
                 <div class="section__top">
                     <p class="section-label mb-16">Comment ça marche ?</p>
-                    <h2 class="section__title title-xl">Simple. Collectif. Efficace.</h2>
+                    <h2 id="howItWorkTitle" class="section__title title-xl">Simple. Collectif. Efficace.</h2>
                     <p class="text--sm">Un processus en trois étapes pour transformer les frustrations du quotidien en actions concrètes</p>
                 </div>
 
@@ -77,19 +66,13 @@
 
         </div>
 
-        <!--------------------------------------------------
-
-            FONCTIONNALTIÉ
-
-         -------------------------------------------------->
-
         <div class="wrapper">
 
-            <section id="fonctionnalite" class="main__section section container">
+            <section id="fonctionality" class="main__section section container" aria-labelledby="fonctionalityTitle">
 
                 <div class="section__top">
                     <p class="section-label mb-16">Fonctionnalités</p>
-                    <h2 class="section__title title-xl">Tout ce qu'il faut, rien de superflu</h2>
+                    <h2 id="fonctionalityTitle" class="section__title title-xl">Tout ce qu'il faut, rien de superflu</h2>
                     <p class="text--sm">FlowFix est conçu pour aller à l'essentiel : faciliter la parole, structurer l'action.</p>
                 </div>
 
@@ -113,19 +96,13 @@
 
         </div>
 
-        <!--------------------------------------------------
-
-            prêt à commencer
-
-         -------------------------------------------------->
-
         <div class="wrapper wrapper--alt">
 
-            <section id="pret-a-commencer" class="main__section section container">
+            <section id="readyToStart" class="main__section section container" aria-labelledby="readyToStartTitle">
 
                 <div class="section__top">
                     <p class="section-label txt-center  mb-16">Prêt à commencer ?</p>
-                    <h2 class=" txt-center section__title title-xl">Donnez la parole à votre équipe</h2>
+                    <h2 id="readyToStartTitle" class=" txt-center section__title title-xl">Donnez la parole à votre équipe</h2>
                     <p class=" txt-center">Créez votre premier groupe en quelques secondes...</p>
                 </div>
 
