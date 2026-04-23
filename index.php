68e9b8616384fc576eba319ddc6d233c29cd8618
Romain CAMENEN <rcamenen@gmail.com>
2026-04-23 10:14:55 +0200

    Retrait du log des erreurs

--- index.php
+++ index.php
@@ -4,9 +4,8 @@ session_start();
 define('ROOT',__DIR__);
 
 ini_set('display_errors', '0');
-ini_set('log_errors', '1');
-ini_set('error_log', __DIR__ . '/errors.log');
-error_reporting(E_ALL);
+ini_set('log_errors', '0');
+error_reporting(0);
 
 use Core\Router;
 use Dotenv\Dotenv;
